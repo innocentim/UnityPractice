07c33773e715eb2f92de81d669435c18b4312fc7
Tim Shen <timshen91@gmail.com>
2015-01-12 01:05:52 -0800

    second

---
+++ Assets/Controller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+	enum Orientation {
+		LEFT,
+		RIGHT,
+	};
+
+	private Orientation orientation;
+
+	// Use this for initialization
+	void Start () {
+		orientation = Orientation.RIGHT;
+	}
+
+	void HandleInput() {
+		if (Input.GetAxis("Horizontal") > 0) {
+			orientation = Orientation.RIGHT;
+		} else if (Input.GetAxis("Horizontal") < 0) {
+			orientation = Orientation.LEFT;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		HandleInput ();
+		UpdateTransientRotate ();
+		UpdateAnimation ();
+	}
+
+	void UpdateTransientRotate() {
+		transform.rotation = Quaternion.Slerp (
+			transform.rotation,
+			orientation == Orientation.RIGHT ? Quaternion.Euler(Vector3.up * 90f) : Quaternion.Euler(Vector3.up * 270f),
+			Time.deltaTime * 10);
+	}
+
+	void UpdateAnimation() {
+	}
+}
