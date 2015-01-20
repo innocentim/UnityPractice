d6aac5ff1f15ae73b6d7b8e84935a9d153c21ded
Tim Shen <timshen91@gmail.com>
2015-01-19 20:35:07 -0800

    4th

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -7,11 +7,13 @@ public class Controller : MonoBehaviour {
 		RIGHT,
 	};
 
-	public float speedFactor;
+	public float maximumSpeed;
+	public float jumpFactor;
 	private Orientation orientation;
-	private float moveSpeed {
+	private float lastSpeedInput = 0f;
+	private bool onTheGround {
 		get {
-			return Input.GetAxis ("Horizontal");
+			return -0.01 < transform.position.y && transform.position.y < 0.01;
 		}
 	}
 
@@ -25,20 +27,30 @@ public class Controller : MonoBehaviour {
 		}
 	}
 
-	void HandleInput() {
-		if (Input.GetAxis("Horizontal") > 0) {
-			orientation = Orientation.RIGHT;
-		} else if (Input.GetAxis("Horizontal") < 0) {
-			orientation = Orientation.LEFT;
-		}
-	}
-
 	// Update is called once per frame
 	void Update () {
 		HandleInput ();
 		UpdateTransientRotate ();
 		UpdateTranslation ();
 		UpdateAnimation ();
+		KeepAboveGround ();
+	}
+
+	void HandleInput() {
+		if (Input.GetAxis("Horizontal") > 0) {
+			orientation = Orientation.RIGHT;
+		} else if (Input.GetAxis("Horizontal") < 0) {
+			orientation = Orientation.LEFT;
+		}
+		if (Input.GetButtonDown ("Jump")) {
+			HandleJump();
+		}
+	}
+
+	void HandleJump() {
+		if (onTheGround) {
+			rigidbody.velocity += Vector3.up * jumpFactor;
+		}
 	}
 
 	void UpdateTransientRotate() {
@@ -49,11 +61,26 @@ public class Controller : MonoBehaviour {
 	}
 
 	void UpdateTranslation() {
-		transform.Translate (Vector3.right * Time.deltaTime * moveSpeed * speedFactor, Space.World);
+		if (onTheGround) {
+			rigidbody.velocity += Vector3.right * (Input.GetAxis("Horizontal") - lastSpeedInput) * maximumSpeed;
+			lastSpeedInput = Input.GetAxis("Horizontal");
+		}
 	}
 
 	void UpdateAnimation() {
-		animation["idle"].weight = 1 - Mathf.Abs (moveSpeed);
-		animation["run"].weight = Mathf.Abs (moveSpeed);
+		animation["idle"].weight = 1 - Mathf.Abs (rigidbody.velocity.x);
+		animation["run"].weight = Mathf.Abs (rigidbody.velocity.x);
+	}
+
+	void KeepAboveGround() {
+		if (transform.position.y < 0 && rigidbody.velocity.y < 0) {
+			var v = Vector3.zero;
+			v.x = transform.position.x;
+			v.z = transform.position.z;
+			transform.position = v;
+			v.x = rigidbody.velocity.x;
+			v.z = rigidbody.velocity.z;
+			rigidbody.velocity = v;
+		}
 	}
 }
